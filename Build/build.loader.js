fe2fb9ab7543ebe6b16a7f37c60cb67e2896adf3
fedi41-prog <crocofedi4@gmail.com>
2026-05-08 09:31:40 +0200

    h

--- Build/build.loader.js
+++ Build/build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57007611,
+    wasmFileSize: 57023333,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
